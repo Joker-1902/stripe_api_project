9e58b8b9eef0ff42ce72740712d5797c562cf1a7
Abubakr <dmx0204@mail.com>
2025-08-02 20:24:20 +0500

    Connecting PostgreSQL

--- payment/settings.py
+++ payment/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import dj_database_url
 from dotenv import load_dotenv
 from pathlib import Path
 import stripe
@@ -85,6 +86,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
+
 
 
 # Password validation
